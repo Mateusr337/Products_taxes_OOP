f5f78e165e9db27041230008192a9627aa701702
Mateus <mateusc.rossetto@hotmail.com>
2022-07-04 17:20:50 -0300

    fix: declaration params of Item class

--- src/Item.ts
+++ src/Item.ts
@@ -1,11 +1,3 @@
-import Order from "./Order";
-
 export default abstract class Item {
-	public description: String;
-	public price: number;
-
-	constructor(description: string, price: number) {
-		this.description = description;
-		this.price = price;
-	}
+	constructor(public description: string, public price: number) {}
 }
